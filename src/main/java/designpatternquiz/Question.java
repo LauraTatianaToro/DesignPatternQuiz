627db03ccfd5e754250e7571ac5ee7f92adf97c1
LauraTatianaToro <lttoro20@gmail.com>
2021-11-07 01:45:01 -0400

    Initial commit

---
+++ src/main/java/designpatternquiz/Question.java
@@ -0,0 +1,48 @@
+package designpatternquiz;
+
+public class Question {
+	//question is composed of one right answers; 3 wrong answers
+	//wrong answers stored in array of strings
+	
+	private String question;
+	private String rightAnswer;
+	private String[] wrongAnswers;
+
+	public Question(String question, String rightAnswer, String[] wrongAnswers) {
+		this.question = question;
+		this.rightAnswer = rightAnswer;
+		this.wrongAnswers = new String[wrongAnswers.length];
+		for (int i = 0; i < wrongAnswers.length; i++) {
+			this.wrongAnswers[i] = wrongAnswers[i];
+		}
+	}
+
+	//getters and setters
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getRightAnswer() {
+		return rightAnswer;
+	}
+
+	public void setRightAnswer(String rightAnswer) {
+		this.rightAnswer = rightAnswer;
+	}
+
+	public String[] getWrongAnswers() {
+		return wrongAnswers;
+	}
+
+	public void setWrongAnswers(String[] wrongAnswers) {
+		this.wrongAnswers = wrongAnswers;
+	}
+	
+	
+
+}
